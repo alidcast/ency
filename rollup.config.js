3e3970d0e01cac30e995b9b6c68abafaca327b46
Alid Castano <alidcastano@gmail.com>
2017-04-17 13:00:55 -0400

    added rollup and configured bundle

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+const replace = require('rollup-plugin-replace')
+const resolve = require('rollup-plugin-node-resolve')
+const babel = require('rollup-plugin-babel')
+const uglify = require('rollup-plugin-uglify')
+const version = process.env.VERSION || require('./package.json').version
+
+module.exports = {
+  entry: 'src/index.js',
+  dest: 'dist/build.js',
+  format: 'umd',
+  moduleName: 'Ency',
+  plugins: [
+    replace({ __VERSION__: version }),
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    uglify()
+  ],
+  banner:
+    `/**
+      * ency v${version}
+      * (c) ${new Date().getFullYear()} Alid Castano
+      * @license MIT
+      */`
+}
